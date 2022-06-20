e1f4595e92edea12bc22d6c5e78394ac46429f97
Ed Chadwick <edwardmchadwick@gmail.com>
2022-06-21 00:08:41 +0100

    better bad input handling
    roll command now tolerant of inappropriately long roll commands or too many dice requested.
    
    Roll command logic now separated into a function - would be useful to somehow map the commands to functions and pass the useful info through

--- HexBug.py
+++ HexBug.py
@@ -5,15 +5,19 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import requests as req
-from dotenv import load_dotenv
+from decouple import config
 import datetime
 import re
 import random
+import time
 
+def log(Message):
+    f = open("hexLog.txt","a")      
+    f.write(str(datetime.datetime.now())+ '   ' + str(Message) + '\n')
+    
 #load the environment variables
 try:
-    load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
+    TOKEN = config('DISCORD_TOKEN')
 except:
     log("Environment file missing or incorrectly formatted.")
     quit()
@@ -25,19 +29,53 @@ prefix = "~"
 client =  Bot(description='', command_prefix=prefix, pm_help = False)
 
 #write logs to a file, create one if it doesn't exist
-def log(Message):
-    f = open("hexLog.txt","a")      
-    f.write(str(datetime.datetime.now())+ '   ' + str(Message) + '\n')
+
 
 
 def roll(die, num=1):
-    tot = 0
     results = {"total":0,"dieRolls":[]}
     for z in range(num):
         chuck = random.randint(1,die)
         results["total"] = results["total"] + chuck
         results["dieRolls"].append([chuck,die])
     return results
+
+def process_dice(equation):
+    total = 0
+    rolls = []
+    nums = []
+    rollSummary = ''
+    error = ''
+    if(re.match('^([0-9]*[dD](4|6|8|10|12|20)\+?)+(\+[0-9]+)*$','+'.join(equation))) and len(equation) <= 10:
+        for eq in equation:
+            if 'd' in eq:
+                die = eq.split('d')
+                if re.match('[0-9]',eq[0]):
+                    if int(die[0]) >=20:
+                        error = 'Too many dice'
+                        return(0,'',error)
+                    rl = roll(int(die[1]),int(die[0]))
+                    total = total + rl["total"]
+
+                else:
+                    rl = roll(int(die[1]))
+                    total = total + rl["total"]
+                for r in rl["dieRolls"]:
+                    rolls.append(r)
+            elif (re.match('^[0-9]+$',eq)):
+                total = total + int(eq)
+                nums.append(eq)
+        for item in rolls:
+            rollSummary = rollSummary + 'D'+str(item[1]) +': '+ str(item[0]) + ' '
+        for n in nums:
+            rollSummary = rollSummary + '+' + eq + ' '
+
+    else:
+        if len(equation) > 10:
+            error = 'Dice Equation too long'
+        else:
+            error = 'Invalid dice equation, please try again.'
+    return(total, rollSummary, error)
 
 #tell the command line HexBug is online!
 @client.event
@@ -63,7 +101,7 @@ async def on_message(message):
         
         #list commands      
         elif(command == (prefix + "help")):
-            await message.channel.send( "Here's what I can do:\n~hi: I'll say hi back!\n~roll n[die]+n[die]....: I'll roll the dice for you!\n~invent: I'll come up with an invention\n~")
+            await message.channel.send( "Here's what I can do:\n~hi: I'll say hi back!\n~roll n[die]+n[die]....: I'll roll the dice for you!\n~invent: I'll come up with an invention\n~flip: I'll flip a coin for you.")
          
          
          #say an affirming statement         
@@ -102,43 +140,33 @@ async def on_message(message):
             await message.channel.send('I am connected to:')
             for s in client.guilds:
                 await message.channel.send(str(s))
-                
+        
+        #flips a coin.
+        elif(command == prefix + "flip"):
+            await message.channel.send('flipping a coin...')
+            log("Flipping a coin for {0} in {1}".format(auth, str(message.guild)))
+            flip = roll(2)
+            time.sleep(1.2)
+            if flip["total"] == 1:
+                await message.channel.send('Heads!')
+                log('Heads')
+            else:
+                await message.channel.send('Tails!')
+                log('Tails')
+            
         #roll the dice. State dice before fixed numbers.
         elif(command == prefix + "roll"):
             equation = ' '.join(args[1:]).lower().replace(" ", "").split("+")
-            print(equation)
-            if(re.match('^([0-9]*[dD](4|6|8|10|12|20)\+?)+(\+[0-9]+)*$','+'.join(equation))):
-                total = 0
-                rolls = []
-                nums = []
-                for eq in equation:
-                    if 'd' in eq:
-                        die = eq.split('d')
-                        if re.match('[0-9]',eq[0]):
-                            rl = roll(int(die[1]),int(die[0]))
-                            total = total + rl["total"]
-
-                        else:
-                            rl = roll(int(die[1]))
-                            total = total + rl["total"]
-                        for r in rl["dieRolls"]:
-                            rolls.append(r)
-                    elif (re.match('^[0-9]+$',eq)):
-                        total = total + int(eq)
-                        nums.append(eq)
-                await message.channel.send('your result is '+ str(total))
-                rollSummary = ''
-                for item in rolls:
-                    rollSummary = rollSummary + 'D'+str(item[1]) +': '+ str(item[0]) + ' '
-                for n in nums:
-                    rollSummary = rollSummary + '+' + eq + ' '
-                await message.channel.send(rollSummary)
+            log("rolling {0} for {1} in {2}".format('+'.join(equation),auth,str(message.guild)))
+            tot, summ, err = process_dice(equation)
+            if len(err) > 0:
+                await message.channel.send(err)
+                log('{0} raised error: {1} in {2}'.format(auth,err,str(message.guild)))
             else:
-                await message.channel.send('Invalid dice equation, please try again.')
-         
-         
+                await message.channel.send('your result is {0}\n{1}'.format(tot,summ))
+                log("Result: {0}".format(summ))
         else:
-            await message.channel.send("Whadda you meeeeaaaan?")
+            await message.channel.send('Command '+ command + ' not found. Try running ~help')
             
     else:
        pass
